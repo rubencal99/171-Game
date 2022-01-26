ca07474a8c6d352865cf4e73b079f30c1e53c482
Dill <dney@ucsc.edu>
2022-01-26 13:16:36 -0800

    reworked spawning and entry colliders

--- Assets/_Scripts/EnemySpanwer.cs
+++ Assets/_Scripts/EnemySpanwer.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class EnemySpanwer : MonoBehaviour
 {
-    public GameObject Enemy;
+    public GameObject[]  Enemies;
     public bool infiniteSpawn = false;
     public bool stopSpawn = false;
     public float spawnTime = 1.0f;
     public float spawnDelay = 1.0f;
     public int numToSpawn = 1;
+    private int enemyCount;
+
+    private bool spawned = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        enemyCount = this.numToSpawn;
+        Debug.Log("initial Enemy count = " + enemyCount);
+       // Enemies = Resources.LoadAll<GameObject>("_Prefabs/Enemies");
+
+
         if(infiniteSpawn)
             InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
         else {
@@ -23,14 +31,48 @@ public class EnemySpanwer : MonoBehaviour
         }
     }
 
+    void Update() {
+        if(spawned)
+            oneStepCloser();
+    }
+
     public void SpawnObject(){
         Vector3 offsetPosition = transform.position;
         offsetPosition.x += Random.Range(-1.5f, 1.5f);
         offsetPosition.y += Random.Range(-1.5f, 1.5f);
-        offsetPosition.z += Random.Range(-1.5f, 1.5f);
-        var clone = Instantiate(Enemy, offsetPosition, Quaternion.identity);
+        var clone = Instantiate(Enemies[Random.Range(0, Enemies.Length)], offsetPosition, Quaternion.identity);
+        clone.transform.parent = this.gameObject.transform;
         if(stopSpawn){
             CancelInvoke("SpawnObject");
         }
+
+        spawned = true;
+
     }
+
+     // Update is called once per frame
+   public void oneStepCloser() {
+      enemyCount = 0;
+       foreach(Transform child in transform)
+       { 
+           if(child.tag == "Enemy") {
+                enemyCount++;
+           }
+             Debug.Log("current enemy count = " + enemyCount); 
+        }
+        if(enemyCount <= 0)
+            checkIfClear();
+       
+   }
+    public void checkIfClear() {
+        foreach(Transform child in transform.parent)
+        {
+        if(child.tag == "Spawner" || child.tag == "Door")
+            Destroy(child.gameObject);
+        }
+
+        Debug.Log("room cleared");
+ 
+    }
+    
 }
--- Assets/_Scripts/EntryCollider.cs
+++ Assets/_Scripts/EntryCollider.cs
@@ -5,35 +5,37 @@ using UnityEngine;
 public class EntryCollider : MonoBehaviour
 {
     // Start is called before the first frame update
+    private bool guarded = false;
 
-    public GameObject spawner;
-    void Start()
-    {
-        
+    void toggleGuarded() {
+        this.guarded = !this.guarded;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other) {
+        if(!guarded) {
         if(other.tag == "Player") {
             this.GetComponent<Collider2D>().isTrigger = true;
             this.transform.GetChild(0).gameObject.SetActive(false);
             Debug.Log("made contact with player");
+            }
         }
     }
 
     void OnTriggerExit2D(Collider2D other) {
 
         if(other.tag == "Player") {
-        spawner.GetComponent<EnemySpanwer>().enabled = true;
        this.GetComponent<Collider2D>().isTrigger = false;
        this.transform.GetChild(0).gameObject.SetActive(true);
-       this.transform.parent.Find("Exit Collider").GetChild(0).gameObject.SetActive(true);
+
+       foreach(Transform child in transform.parent) {
+           if(child.tag == "Door"){
+                   child.GetChild(0).gameObject.SetActive(true);
+                   child.GetComponent<EntryCollider>().toggleGuarded();
+           }
+           if(child.tag == "Spawner")
+                child.GetComponent<EnemySpanwer>().enabled = true;
+              }
+        }
+    //    this.transform.parent.Find("Exit Collider").GetChild(0).gameObject.SetActive(true);
         Debug.Log("leaving collider");
-          }
     }
 }
