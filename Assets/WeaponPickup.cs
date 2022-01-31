8d1fa504826979c2badcf69ae314b617664eec0e
monochromat <efrainluengas2@gmail.com>
2022-01-30 17:12:33 -0800

    Made the Assault rifle pickup script into a general script for any weapon.

--- Assets/WeaponPickup.cs
+++ Assets/WeaponPickup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultRiflePickup : MonoBehaviour
+public class WeaponPickup : MonoBehaviour
 {
     public GameObject FireArm;
 
