916cc9f68c11b7b2085ed30e76f1976bcadc85c8
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-02-15 08:06:06 -0800

    Updated FMOD banks, installed events into prefabs

--- Assets/_Scripts/AgentAudioPlayer.cs
+++ Assets/_Scripts/AgentAudioPlayer.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 // NOTE: It might be better to seperate this class into individual audio players
 // because the required Audio Source component might only play one clip at a time
-public class AgentAudioPlayer : AudioPlayer
+public class AgentAudioPlayer : MonoBehaviour
 {
-    [SerializeField]
-    protected AudioClip damageTakenClip;
-
-    [SerializeField]
-    protected AudioClip deathClip;
-
-    [SerializeField]
-    protected AudioClip stepClip;
+    public FMODUnity.EventReference damageTakenEvent;
+    public FMODUnity.EventReference deathEvent;
+    public FMODUnity.EventReference stepEvent;
 
     public void PlayStepSound()
     {
-        PlayClipWithvariablePitch(stepClip);
+        FMODUnity.RuntimeManager.PlayOneShot(stepEvent, transform.root.position);
     }
 
     public void PlayDamageSound()
     {
-        PlayClipWithvariablePitch(damageTakenClip);
+        FMODUnity.RuntimeManager.PlayOneShot(damageTakenEvent, transform.root.position);
     }
 
     public void PlayDeathSound()
     {
-        PlayClip(deathClip);
+        FMODUnity.RuntimeManager.PlayOneShot(deathEvent, transform.root.position);
     }
 }
