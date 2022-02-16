a2328d9dfee1540548fb492f7084a2456edbcb8b
Alex Rugama <alrugama@ucsc.edu>
2022-02-16 12:41:42 -0800

    revolver added a child of weaponparent but need muzzle and sound

---
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+    public PlayerMovement PlayerMovement;
+    public CapsuleCollider2D Collider;
+
+    private void Start()
+    {
+        PlayerMovement = GetComponent<PlayerMovement>();
+        Collider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // The Vector2 corresponds to the magnitude of movement in the (x, y)    wasd
+    // (0, 0), (0, 1), (1, 0), (1, 1), (0, -1), (-1, 0), (-1, -1), (1, -1), (-1, 1)
+    // Passes the Vector2 to AgentMovement.MoveAgent hence SerializeField
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnMovementKeyPressed { get; set; }
+
+    // Vector2 coresponds to the position of the mouse on the screen
+    // This funciton is used to aim the weapon and change player direction
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnPointerPositionChange { get; set; }
+
+    // Dodge Mechanic
+    // Vector2 Corresponds towards the movement of where the dodge roll happens
+    // *************************
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnDodgeKeyPressed { get; set; }
+    // *************************
+
+    // Calls PlayerWeapon.shoot
+    [field: SerializeField]
+    public UnityEvent OnFireButtonPressed { get; set; }
+
+    // Calls PlayerWeapon.StopShooting
+    [field: SerializeField]
+    public UnityEvent OnFireButtonReleased { get; set; }
+
+    // Calls PlayerWeapon.StopShooting
+    [field: SerializeField]
+    public UnityEvent OnReloadButtonPressed { get; set; }
+
+    // Calls Player.ThowItem
+    [field: SerializeField]
+    public UnityEvent OnThrowButtonPressed { get; set; }
+
+    // Calls SceneManager.RestartScene
+    [field: SerializeField]
+    public UnityEvent OnRestartButtonPressed { get; set; }
+
+    // Calls SceneManager.RestartScene
+    [field: SerializeField]
+    public UnityEvent OnRespawnButtonPressed { get; set; }
+
+    // Calls PlayerWeapon.UseMelee
+    [field: SerializeField]
+    public UnityEvent OnMeleeButtonPressed { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent OnStandButtonPressed { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent OnInteractKeyPressed { get; set; }
+
+    [field: SerializeField]
+    public UnityEvent<Vector2> OnHookKeyPressed { get; set; }
+}
---
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : AgentMovement
+{
+    // Modifyable field for Dodge
+    // **************
+    [SerializeField]
+    protected float dodgeVelocity = 200;
+
+    protected PlayerStateManager PlayerState;
+
+    protected void Start()
+    {
+        PlayerState = GetComponent<PlayerStateManager>();
+    }
+
+
+    // this function integrates acceleration
+    protected override float calculateSpeed(Vector2 movementInput)
+    {
+        if (movementInput.magnitude > 0)
+        {
+            currentVelocity += MovementData.acceleration * Time.deltaTime;
+        }
+        else
+        {
+            currentVelocity -= MovementData.decceleration * Time.deltaTime;
+        }
+        // Check if player is dodging
+        // Debug.Log("Diving = " + PlayerState.DiveState.diving);
+        if (PlayerState.DiveState.diving == true) {
+            Debug.Log("In Dive velocity");
+            return Mathf.Clamp((currentVelocity * dodgeVelocity), 0, MovementData.maxDodgeSpeed);
+        }
+        // Returns velocity between 0 and maxSpeed
+        return Mathf.Clamp(currentVelocity, 0, MovementData.maxRunSpeed);
+    }
+
+    //********** Dodge function
+    // Should player be able to dodge when not moving??
+    public void dodge(Vector2 dodgeDirection) {
+        rigidbody2D.velocity = Vector2.zero; // set speed to zero
+        rigidbody2D.velocity += dodgeDirection * dodgeVelocity; // create dodge
+        Debug.Log("Dodge Velocity: " + rigidbody2D.velocity);
+    }
+
+    public void ResetSpeed()
+    {
+        currentVelocity = 0;
+    }
+
+    public void hook( ){
+
+    }
+}
