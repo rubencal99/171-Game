2bb4ed8d378670e96c3d9a17253fccda7a1604e8
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-02-27 22:46:46 -0800

    added temp augment ui, implemented dragdrop system

---
+++ Assets/_Scripts/UI-Augmentation/InventorySlot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData) {
+        Debug.Log("put me in pUT ME THE FUCK IN");
+        if (eventData.pointerDrag != null) {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+
+    }
+}
