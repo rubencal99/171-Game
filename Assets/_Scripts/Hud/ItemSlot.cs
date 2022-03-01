dec1eb39bb8770f46fcf73091cd8536bc51d89c0
zlu35 <63579313+zlu35@users.noreply.github.com>
2022-02-28 16:37:54 -0800

    Floating window for Shop UI implement
    when mouse is pointing at the slots, the floating window appear.
    when mouse leave the slots, floating window disappear.

---
+++ Assets/_Scripts/Hud/ItemSlot.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject tooltip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // tooltip.SetActive(true);
+        tooltip.SetActive(true);
+        // ToolTip.Show();
+        // Debug.Log("Show Floating window");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.SetActive(false);
+        // ToolTip.Hide();
+        // Debug.Log("Hide Floating window");
+    }
+
+}
