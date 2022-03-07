829dfcbe7633848c24438093c6ccf8ad9438a1bb
Dill <dney@ucsc.edu>
2022-03-07 08:41:49 -0800

    should be on another branch

--- Assets/_Scripts/EntryCollider.cs
+++ Assets/_Scripts/EntryCollider.cs
@@ -12,7 +12,8 @@ public class EntryCollider : MonoBehaviour
 
        // boxCol = GetComponent<BoxCollider2D>();
         RoomNode room = this.transform.parent.GetComponent<RoomNode>();
-        this.gameObject.transform.localScale = new Vector3((float)room.length - 1f, (float)room.width - 1f, 1f);
+        this.transform.position = new Vector3((float)room.roomCenter.x +0.5f , (float)room.roomCenter.y + 1f, 0f);
+        this.gameObject.transform.localScale = new Vector3((float)room.length , (float)room.width , 0f);
     }
     
     private bool guarded = false;
@@ -27,7 +28,7 @@ public class EntryCollider : MonoBehaviour
             //this.GetComponent<Collider2D>().isTrigger = false;
             //this.transform.GetChild(0).gameObject.SetActive(true);
             this.gameObject.SetActive(false);
-           //  Debug.Log("leaving collider");
+             Debug.Log("in collider");
             }
      //   }
     }
