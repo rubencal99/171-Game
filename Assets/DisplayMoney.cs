47b27f45ae53fb5c424f8c85279c21ebececb161
zlu35 <63579313+zlu35@users.noreply.github.com>
2022-03-11 15:10:32 -0800

    Add money display on shop UI

--- Assets/DisplayMoney.cs
+++ Assets/DisplayMoney.cs
@@ -13,6 +13,7 @@ public class DisplayMoney : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      obj = GameObject.Find("Player");
            p = obj.GetComponent<Player>();
          // 
     }
