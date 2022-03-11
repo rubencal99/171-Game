fae540e9532b67a8e09132e739370cf10cb5606a
monochromat <efrainluengas2@gmail.com>
2022-03-10 18:20:00 -0800

    fixed pickup bug again

--- Assets/_Scripts/WeaponPickup.cs
+++ Assets/_Scripts/WeaponPickup.cs
@@ -34,7 +34,7 @@ public class WeaponPickup : MonoBehaviour
                
                 GameObject thisFireArm = Instantiate(FireArm) as GameObject;
                 thisFireArm.transform.parent = GameObject.Find("WeaponParent").transform;
-                thisFireArm.transform.localPosition = new Vector3(0f, 0f, 0f);
+                thisFireArm.transform.localPosition = new Vector3(0f, -0.25f, 0f);
                 thisFireArm.transform.localRotation = Quaternion.identity;
                 thisFireArm.SetActive(false);
                  popup popup = FindObjectOfType<popup>();
