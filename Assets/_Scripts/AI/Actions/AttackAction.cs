a731cb3326afe1c12026677adf590efe851868c3
d-ney <46723408+d-ney@users.noreply.github.com>
2022-03-11 16:12:14 -0800

    garbie

--- Assets/_Scripts/AI/Actions/AttackAction.cs
+++ Assets/_Scripts/AI/Actions/AttackAction.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class AttackAction : AIAction
 {
+    GameObject self;
+     void Start(){
+        self = this.transform.root.gameObject;
+    }
     public override void TakeAction()
     {
         // aiMovementData.Direction = Vector2.zero;
@@ -11,6 +15,7 @@ public class AttackAction : AIAction
         // enemyBrain.Move(aiMovementData.Direction);
         enemyBrain.Aim(aiMovementData.PointOfInterest);
         aiActionData.Attack = true;
+         self.GetComponentInChildren<AgentAnimations>().SetAttackAnimation();
         enemyBrain.Attack();
         //enemyBrain.StopAttack();
     }
