ffb18e74a59554e2e1edda29a35cc01b71efae4d
Alex Rugama <alrugama@ucsc.edu>
2022-03-11 23:39:46 -0800

    runs oneshotplay

--- Assets/ShopSoundManager.cs
+++ Assets/ShopSoundManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ShopSoundManager : MonoBehaviour
 {
+
+    private FMOD.Studio.EventInstance instance;
+
+    public FMODUnity.EventReference fmodEvent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        //instance.start();
+        FMODUnity.RuntimeManager.PlayOneShot(fmodEvent, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if(Shop.inShop){
+            //instance.setParameterByName("inShop", 1);
+            FMODUnity.RuntimeManager.PlayOneShot(fmodEvent, transform.position);
+        }
+        else
+            instance.setParameterByName("inShop", 0);
+        */
+    }
+
+    void OnDestroy(){
+        //instance.release();
     }
 }
