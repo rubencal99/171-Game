5ad6fc6ab0aa07cf137a04b23580618d6e4dc7c7
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-03-12 19:56:56 -0800

    Updated FMOD banks

--- Assets/BackgroundSoundManager.cs
+++ Assets/BackgroundSoundManager.cs
@@ -14,7 +14,8 @@ public class BackgroundSoundManager : MonoBehaviour
 
     // Update is called once per frame
  
-    void OnCollisionEnter2D(Collider2D collider){
+    void OnCollisionEnter2D(Collider2D collider)
+    {
         
     }
 }
