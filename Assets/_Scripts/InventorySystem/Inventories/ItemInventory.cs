d0a53e47830ecfb00985f8afcd2d436ba2456fb6
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-04-01 18:48:05 -0700

    created ItemInventory SO

--- Assets/_Scripts/InventorySystem/Inventories/ItemInventory.cs
+++ Assets/_Scripts/InventorySystem/Inventories/ItemInventory.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemInventory
+[CreateAssetMenu(fileName = "New Inventory", menuName = "Inventory System/Inventory")]
+public class ItemInventory : ScriptableObject
 {
-    private List<ItemObject> itemList;
-
-    public ItemInventory() {
-        itemList = new List<ItemObject>(); 
-
-        Debug.Log("Inventory"); 
+    public List<ItemObject> Container = new List<ItemObject>();
+    public void AddItem(ItemObject _item, int _amount)
+    {
+        // check if item is already in inventory
+        // assume we don't
+        bool hasItem = false;
+        for (int i = 0; i < Container.Count; i++)
+        {
+            if(Container[i].item == _item)
+            {
+                Container[i].AddAmount(_amount);
+                hasItem = true;
+                break;
+            }
+        }
+        // if after all that business still no item, put it in a new slot
+        if (!hasItem)
+        {
+            Container.Add(new InventorySlot(_item, _amount));
+        }
     }
+}
 
-
+[System.Serializable]
+public class InventorySlot
+{
+    public ItemObject item;
+    public int amount;
+    public InventorySlot(ItemObject _item, int _amount)
+    {
+        item = _item;
+        amount = _amount;
+    }
+    public void AddAmount(int value)
+    {
+        amount += value;
+    }
 }
