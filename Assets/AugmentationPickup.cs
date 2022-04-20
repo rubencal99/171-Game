aedec6577dd73650ea8f7ab624d24984361d3ecd
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-04-20 04:03:15 -0700

    cleaned up AugmentationPickup script

--- Assets/AugmentationPickup.cs
+++ Assets/AugmentationPickup.cs
@@ -8,23 +8,11 @@ public class AugmentationPickup : MonoBehaviour
 
     private void OnTriggerEnter(Collider col)
     {
-        Debug.Log("Collided with object " + col.tag);
+        //Debug.Log("Collided with object " + col.tag);
         if (col.tag == "Player")
         {
             Player.instance.inventory.AddItemToInventory(augmentation, 1);
             Destroy(gameObject);
         }
     } 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
