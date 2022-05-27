0e78e6378a6503bcd5ada1485b4ebfbc6ef2c860
alachoff-ucsc <74936465+alachoff-ucsc@users.noreply.github.com>
2022-05-26 19:57:13 -0700

    added sfx to item dropping

--- Assets/_Scripts/InventorySystem/UI-Augmentation/InventoryTrash.cs
+++ Assets/_Scripts/InventorySystem/UI-Augmentation/InventoryTrash.cs
@@ -6,17 +6,22 @@ using UnityEngine.EventSystems;
 public class InventoryTrash : MonoBehaviour, IDropHandler
 {
     public GameObject p;
+    public InventoryUIParent UIParent;
+    private InventorySoundManager soundManager;
     void Start()
     {
         // find player
         p = Player.instance.gameObject;
+        soundManager = UIParent.GetComponent<InventorySoundManager>();
 
     }
     public void OnDrop(PointerEventData eventData)
     {
         // call DropItem
         DragDrop _dragdrop = eventData.pointerDrag.GetComponent<DragDrop>();
+        soundManager.PlayClickItemSound((float)_dragdrop.slotElement.slot.item.type, 0);
         CreateDrop(_dragdrop.slotElement.slot);
+        
     }
     void CreateDrop(Slot _slot)
     {
